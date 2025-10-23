7df363fc1d97665708f3dafbfc0749f32676a95a
luigi-casassi <luigicasassi@outlook.com>
2025-10-23 00:38:33 +0000

    modifica a frase do site

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Página inicial do meu primeiro site, obrigado por visualizar</h1>
+    return <h1>Página inicial do meu primeiro site, obrigado por visualizar🤙</h1>
 }
 
 export default Home;
